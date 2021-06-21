ab7fc9cb95fa3a665f59fe1078df35f66c45154d
eunyechan <glqgkq384@naver.com>
2021-06-21 18:57:08 +0900

    유튜브 클론 업데이트

---
+++ README.md
@@ -0,0 +1,15 @@
+# wetube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Logion
+/search -> Search
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comment -> Comment on a video
+/videos/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,22 +1,25 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const home = (req, res) => {
-  console.log("I will respond");
   return res.send("hello");
 };
 const login = (req, res) => {
   return res.send("login");
 };
-
-app.use(logger);
-app.get("/", home);
-app, get("/login", login);
 
 const handleListening = () =>
   console.log("📢Server listenting on port http://localhost:${PORT}💨");
