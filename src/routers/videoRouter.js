aa9de936d0514d468ea2f2dcfe022602d93db4cd
eunyechan <glqgkq384@naver.com>
2021-07-14 17:56:01 +0900

    mongoDB

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,8 +1,8 @@
 import express from "express";
 import {
   watch,
-  upload,
-  deleteVideo,
+  getUpload,
+  postUpload,
   getEdit,
   postEdit,
 } from "../controllers/videoController";
@@ -10,7 +10,7 @@ import {
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", getEdit);
-videoRouter.post("/:id(\\d+)/edit", postEdit);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
