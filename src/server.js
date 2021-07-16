b3b519c9a508933378bf695ab731a54d4bef1da4
eunyechan <glqgkq384@naver.com>
2021-07-16 19:22:31 +0900

    callback promise

--- src/server.js
+++ src/server.js
@@ -1,12 +1,8 @@
-import "./db";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -19,14 +15,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const home = (req, res) => {
-  return res.send("hello");
-};
-const login = (req, res) => {
-  return res.send("login");
-};
-
-const handleListening = () =>
-  console.log("📢Server listenting on port http://localhost:${PORT}💨");
-
-app.listen(PORT, handleListening);
+export default app;
