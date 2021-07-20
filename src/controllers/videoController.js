06fbd4ff30ff7d2bd93245e5293d7832543c003d
eunyechan <glqgkq384@naver.com>
2021-07-20 17:20:15 +0900

    업데이트

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,27 +1,29 @@
 import Video from "../models/Video";
 
-/*
-  Video.find({}, (error, videos) => {
-    if(error){
-     return res.render("server-error")
-    }
-    return res.render("home", {pageTitle: "Home", vudeos})
-  });
-*/
-
 export const home = async (req, res) => {
   const videos = await Video.find({});
   console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => {
+
+export const watch = async (req, res) => {
   const { id } = req.params;
-  res.render("watch", { pageTitle: `Watching` });
+  const video = await Video.findById(id);
+  if (video === null) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  res.render("watch", { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  res.render("edit", { pageTitle: `Edit ${video.title}`, video });
 };
+
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
@@ -46,7 +48,7 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    return res.sender("upload", {
+    return res.render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
