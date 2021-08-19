18af0c33f8f8012972f9e4ab63439d05048e95b9
eunyechan <glqgkq384@naver.com>
2021-08-19 19:52:25 +0900

    scss 시작

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,17 +16,17 @@
   \*******************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./src/client/scss/styles.scss\");\n\nalert(\"it works\");\n\n//# sourceURL=webpack://wetube/./src/client/js/main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_screens_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/screens/styles.scss */ \"./src/client/scss/screens/styles.scss\");\n\nconsole.log(\"it works\");\n\n//# sourceURL=webpack://wetube/./src/client/js/main.js?");
 
 /***/ }),
 
-/***/ "./src/client/scss/styles.scss":
-/*!*************************************!*\
-  !*** ./src/client/scss/styles.scss ***!
-  \*************************************/
+/***/ "./src/client/scss/screens/styles.scss":
+/*!*********************************************!*\
+  !*** ./src/client/scss/screens/styles.scss ***!
+  \*********************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://wetube/./src/client/scss/styles.scss?");
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://wetube/./src/client/scss/screens/styles.scss?");
 
 /***/ })
 
--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,2 +1,2 @@
-import "../scss/styles.scss";
-alert("it works");
+import "../scss/screens/styles.scss";
+console.log("it works");
