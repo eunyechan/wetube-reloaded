baa05b5c48eef7a5a7987788746e4d0fecc971a6
eunyechan <glqgkq384@naver.com>
2021-10-12 20:33:11 +0900

    udpate

--- src/server.js
+++ src/server.js
@@ -1,57 +1,54 @@
+import "./init";
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
 import flash from "express-flash";
 import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
-import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 import { localsMiddleware } from "./middlewares";
 import apiRouter from "./routers/apiRouter";
 
 const app = express();
-const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
-app.use(logger);
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// 클라이언트에서 보낸 데이터를 JSON.parse를 수행하여 서버에 전달
 
 app.use(
   session({
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
+    // cookie: {
+    //   maxAge: 20000,
+    // },
     store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
+app.use(flash());
+app.use(localsMiddleware);
+///////////////////////////////////////////////////////////////////
+app.use("/core", express.static("node_modules/@ffmpeg/core/dist"));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
 });
 
-[
-  {
-    AllowedHeaders: ["*"],
-    AllowedMethods: ["GET", "PUT", "POST", "DELETE"],
-    AllowedOrigins: ["https://wetubecloneapp.herokuapp.com/"],
-    ExposeHeaders: [],
-  },
-];
-
-app.use(flash());
-app.use(localsMiddleware);
+///////////////////////////////////////////////////////////////////
 app.use("/uploads", express.static("uploads"));
+// 서버의 assets 폴더를 브라우저의 URL /static을 통해서 공개
 app.use("/static", express.static("assets"));
+
 app.use("/", rootRouter);
-app.use("/videos", videoRouter);
 app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 app.use("/api", apiRouter);
 
 export default app;
