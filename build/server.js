ad9f1abb63b169a3bd02f689a9edf3c39a65a38c
eunyechan <glqgkq384@naver.com>
2021-10-12 21:20:11 +0900

    upadate

--- build/server.js
+++ build/server.js
@@ -5,8 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
-require("./init");
-
 var _express = _interopRequireDefault(require("express"));
 
 var _morgan = _interopRequireDefault(require("morgan"));
@@ -19,9 +17,9 @@ var _connectMongo = _interopRequireDefault(require("connect-mongo"));
 
 var _rootRouter = _interopRequireDefault(require("./routers/rootRouter"));
 
-var _userRouter = _interopRequireDefault(require("./routers/userRouter"));
-
 var _videoRouter = _interopRequireDefault(require("./routers/videoRouter"));
+
+var _userRouter = _interopRequireDefault(require("./routers/userRouter"));
 
 var _middlewares = require("./middlewares");
 
@@ -30,41 +28,40 @@ var _apiRouter = _interopRequireDefault(require("./routers/apiRouter"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var app = (0, _express["default"])();
+var logger = (0, _morgan["default"])("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
-app.use((0, _morgan["default"])("dev"));
+app.use(logger);
 app.use(_express["default"].urlencoded({
   extended: true
 }));
-app.use(_express["default"].json()); // 클라이언트에서 보낸 데이터를 JSON.parse를 수행하여 서버에 전달
-
+app.use(_express["default"].json());
 app.use((0, _expressSession["default"])({
   secret: process.env.COOKIE_SECRET,
   resave: false,
   saveUninitialized: false,
-  // cookie: {
-  //   maxAge: 20000,
-  // },
   store: _connectMongo["default"].create({
     mongoUrl: process.env.DB_URL
   })
 }));
-app.use((0, _expressFlash["default"])());
-app.use(_middlewares.localsMiddleware); ///////////////////////////////////////////////////////////////////
-
-app.use("/core", _express["default"]["static"]("node_modules/@ffmpeg/core/dist"));
 app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
-}); ///////////////////////////////////////////////////////////////////
-
-app.use("/uploads", _express["default"]["static"]("uploads")); // 서버의 assets 폴더를 브라우저의 URL /static을 통해서 공개
-
+});
+app.use((0, _expressFlash["default"])());
+app.use(_middlewares.localsMiddleware);
+app.use("/uploads", _express["default"]["static"]("uploads"));
 app.use("/static", _express["default"]["static"]("assets"));
 app.use("/", _rootRouter["default"]);
-app.use("/users", _userRouter["default"]);
 app.use("/videos", _videoRouter["default"]);
+app.use("/users", _userRouter["default"]);
 app.use("/api", _apiRouter["default"]);
 var _default = app;
 exports["default"] = _default;
