fa1cba093641dd823a711e46195b27fd272c397e
eunyechan <glqgkq384@naver.com>
2021-10-25 17:08:44 +0900

    hash join

--- src/models/Video.js
+++ src/models/Video.js
@@ -17,10 +17,9 @@ const videoSchema = new mongoose.Schema({
 });
 
 videoSchema.static("formatHashtags", function (hashtags) {
-  const space = /|s/g;
   return hashtags
     .split(",")
-    .map((word) => (word.startsWith(`#${space}`) ? word : `#${word}`));
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
 
 const Video = mongoose.model("Video", videoSchema);
