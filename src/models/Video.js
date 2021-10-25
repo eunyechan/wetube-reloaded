0aef4666256804a3195b46a139bf4566720c4f07
eunyechan <glqgkq384@naver.com>
2021-10-25 15:04:12 +0900

    update hash

--- src/models/Video.js
+++ src/models/Video.js
@@ -17,9 +17,11 @@ const videoSchema = new mongoose.Schema({
 });
 
 videoSchema.static("formatHashtags", function (hashtags) {
-  return hashtags
-    .split(",")
-    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+  return (
+    hashtags
+      // .split(",")
+      .map((word) => (word.startsWith("#") ? word : `#${word}`))
+  );
 });
 
 const Video = mongoose.model("Video", videoSchema);
