79e9bdff85fcb028b41ecae9ff7cb910359c9cdd
eunyechan <glqgkq384@naver.com>
2021-10-25 14:42:47 +0900

    update user-profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -169,7 +169,7 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? (isHeroki ? file.location : file.path) : avatarUrl,
+      avatarUrl: file ? (isHeroku ? file.location : file.path) : avatarUrl,
       name,
       email,
       username,
